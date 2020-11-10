021c5f697bf1ed13210edc8b454205070467b7d7
turpp <white.tripp@icloud.com>
2020-11-10 12:56:58 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do 
+        erb :newteam
+        
+    end
 
+    post '/team' do
+    @name=params[:name]
+    @pg=params[:pg]
+    @sg=params[:sg]
+    @sf=params[:sf]
+    @pf=params[:pf]
+    @c=params[:c]
+    @coach=params[:coach]
+
+    erb :team
+    end
 
 end
